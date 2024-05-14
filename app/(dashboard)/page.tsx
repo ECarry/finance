57f615a4f957c1b99279736e5d5fb79a3338ef3b
ECarry <lianshiliang93@gmail.com>
2024-05-15 00:12:24 +0800

    chore: account GET api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+  const { data: accounts, isLoading } = useGetAccounts();
+
   return (
     <div>
-      <UserButton afterSwitchSessionUrl="/" />
+      {accounts?.map((account) => (
+        <div key={account.id}>{account.name}</div>
+      ))}
     </div>
   );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,23 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "Not authenticated",
-    });
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
   }
 
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
+  return c.json({ error: "Internal error" }, 500);
 });
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
